d33fb9e8f7742d9ac4d6e32984ba2b27b8bb99eb
Gowthams1999-commits <githubgowtham1999@gmail.com>
2025-10-09 21:01:08 +0530

    Create provider.tf

---
+++ variable/string/provider.tf
@@ -0,0 +1,21 @@
+##Terraform block
+
+terraform {
+
+  required_providers {
+
+    aws = {
+
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+
+    }
+  }
+}
+
+provider "aws" {
+
+
+  region = var.aws_region
+
+}
