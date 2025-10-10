12459a03a39aafe9207c085a1ba2f8b8001f45af
Gowthams1999-commits <githubgowtham1999@gmail.com>
2025-10-10 10:34:29 +0530

    Create ec2.tf

---
+++ variable/map/ec2.tf
@@ -0,0 +1,92 @@
+## variable blocks
+
+
+variable "aws_region" {
+
+  description = "region for your aws resources"
+  type        = string
+
+}
+
+variable "aws_ami_id" {
+
+  description = "ami id for ec2 instance"
+  type        = string
+
+
+}
+
+
+# validation block
+variable "aws_instance_type" {
+
+  description = "instance type for ec2"
+  type        = string
+
+  validation {
+
+    condition     = var.aws_instance_type == "t2.micro" || var.aws_instance_type == "t2.large"
+    error_message = "only support for instance type t2.micro and t2.large"
+
+  }
+
+}
+
+
+
+## Objects
+
+variable "volume_config" {
+
+  description = ""
+  type = object({
+
+    volume_size           = number
+    volume_type           = string
+    delete_on_termination = bool
+
+
+
+  })
+
+}
+
+
+#map
+
+variable "ec2_tag" {
+
+
+  type    = map(string)
+  default = {}
+
+}
+
+## ec2 instance creation
+
+resource "aws_instance" "demo_server_D" {
+
+  ami           = var.aws_ami_id
+  instance_type = var.aws_instance_type
+
+  tags = merge(
+
+
+    {
+
+      name = "demo_server_D"
+
+    },
+
+    var.ec2_tag
+  )
+  root_block_device {
+
+    delete_on_termination = var.volume_config.delete_on_termination
+    volume_size           = var.volume_config.volume_size
+    volume_type           = var.volume_config.volume_type
+
+
+  }
+
+}
